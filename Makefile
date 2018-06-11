373144052915bd9ecbdc93532b5876ceee11bfcb
Rene Peinthor <rene.peinthor@linbit.com>
2018-06-11 08:48:01 +0200

    add setup.py, makefile and debian package files

---
+++ Makefile
@@ -0,0 +1,81 @@
+GIT = git
+INSTALLFILES=.installfiles
+PYTHON = python2
+override GITHEAD := $(shell test -e .git && $(GIT) rev-parse HEAD)
+
+U := $(shell $(PYTHON) ./setup.py versionup2date >/dev/null 2>&1; echo $$?;)
+TESTS = $(wildcard unit-tests/*_test.py)
+
+all: doc
+	$(PYTHON) setup.py build
+
+install: linstor/consts_githash.py
+	$(PYTHON) setup.py install --record $(INSTALLFILES)
+
+uninstall:
+	test -f $(INSTALLFILES) && cat $(INSTALLFILES) | xargs rm -rf || true
+	rm -f $(INSTALLFILES)
+
+ifneq ($(U),0)
+up2date:
+	$(error "Update your Version strings/Changelogs")
+else
+up2date: linstor/consts_githash.py
+	$(info "Version strings/Changelogs up to date")
+endif
+
+.PHONY: linstor/drbdsetup_options.py
+linstor/drbdsetup_options.py:
+	linstor-common/gendrbdoptions.py python $@
+
+release: up2date clean
+	$(PYTHON) setup.py sdist
+	@echo && echo "Did you run distclean?"
+	@echo && echo "Did you generate and commit the latest drbdsetup options?"
+
+debrelease:
+	echo 'recursive-include debian *' >> MANIFEST.in
+	dh_clean || true
+	make release
+	git checkout MANIFEST.in
+
+.PHONY: gensrc
+gensrc:
+	make -C linstor-common cleanpython
+	make -C linstor-common python
+
+# no gensrc here, that is in debian/rules
+deb: up2date
+	[ -d ./debian ] || (echo "Your checkout/tarball does not contain a debian directory" && false)
+	debuild -i -us -uc -b
+
+# it is up to you (or the buildenv) to provide a distri specific setup.cfg
+rpm: up2date gensrc
+	$(PYTHON) setup.py bdist_rpm
+
+.PHONY: linstor/consts_githash.py
+ifdef GITHEAD
+override GITDIFF := $(shell $(GIT) diff --name-only HEAD 2>/dev/null | \
+			grep -vxF "MANIFEST.in" | \
+			tr -s '\t\n' '  ' | \
+			sed -e 's/^/ /;s/ *$$//')
+linstor/consts_githash.py:
+	@echo "GITHASH = 'GIT-hash: $(GITHEAD)$(GITDIFF)'" > $@
+else
+linstor/consts_githash.py:
+	@echo >&2 "Need a git checkout to regenerate $@"; test -s $@
+endif
+
+md5sums:
+	CURDATE=$$(date +%s); for i in $$(${GIT} ls-files | sort); do md5sum $$i >> md5sums.$${CURDATE}; done
+
+clean:
+	$(PYTHON) setup.py clean
+	rm -f man-pages/*.gz
+
+distclean: clean
+	git clean -d -f || true
+
+check:
+	# currently none
+	# $(PYTHON) $(TESTS)
---
+++ setup.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python2
+"""
+    linstor - management of distributed DRBD9 resources
+    Copyright (C) 2013 - 2018  LINBIT HA-Solutions GmbH
+    Author: Robert Altnoeder, Philipp Reisner
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
+import os
+import sys
+from setuptools import setup, Command
+
+
+def get_version():
+    from linstor_client.consts import VERSION
+    return VERSION
+
+
+class CheckUpToDate(Command):
+    description = "Check if version strings are up to date"
+    user_options = []
+
+    def initialize_options(self):
+        self.cwd = None
+
+    def finalize_options(self):
+        self.cwd = os.getcwd()
+
+    def run(self):
+        version = get_version()
+        try:
+            with open("debian/changelog") as f:
+                firstline = f.readline()
+                if version not in firstline:
+                    # returning false is not promoted
+                    sys.exit(1)
+        except IOError:
+            # probably a release tarball without the debian directory but with Makefile
+            return True
+
+
+setup(
+    name="linstor",
+    version='0.2.0',
+    description="Linstor python api",
+    long_description="Python linstor api interface",
+    url='https://www.linbit.com',
+    author="Robert Altnoeder <robert.altnoeder@linbit.com>, Roland Kammerer <roland.kammerer@linbit.com>" +
+           ", Rene Peinthor <rene.peinthor@linbit.com>",
+    author_email="roland.kammerer@linbit.com",
+    maintainer="LINBIT HA-Solutions GmbH",
+    maintainer_email="drbd-user@lists.linbit.com",
+    license="GPLv3",
+    install_requires=['protobuf'],
+    packages=['linstor'],
+    # package_data={},
+    cmdclass={
+        "versionup2date": CheckUpToDate
+    },
+    test_suite="tests"
+)
