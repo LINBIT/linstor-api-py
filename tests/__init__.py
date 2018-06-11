2945054d665a48bb3862fa9934db21138b74663b
Rene Peinthor <rene.peinthor@linbit.com>
2018-06-11 14:49:33 +0200

    add api unittests moved from linstor_client

---
+++ tests/__init__.py
@@ -0,0 +1,27 @@
+import unittest
+from . import test_utils
+
+_std_tests = [
+    "tests.test_utils"
+]
+
+
+def load_tests(loader, tests, pattern):
+    suite = unittest.TestSuite()
+    loaded_tests = loader.loadTestsFromNames(_std_tests)
+    suite.addTest(loaded_tests)
+    return suite
+
+
+def load_all():
+    suite = unittest.TestSuite()
+    loaded_tests = unittest.defaultTestLoader.loadTestsFromNames(_std_tests)
+    suite.addTest(loaded_tests)
+    return suite
+
+
+def test_without_controller():
+    suite = unittest.TestSuite()
+    loaded_tests = unittest.defaultTestLoader.loadTestsFromNames(_std_tests)
+    suite.addTest(loaded_tests)
+    return suite
