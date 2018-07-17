8529625ed43ee4cb215de9f925d56882dfc9d368
Rene Peinthor <rene.peinthor@linbit.com>
2018-07-17 11:02:43 +0200

    python-linstor version 0.2.2

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ class CheckUpToDate(Command):
 
 setup(
     name="python-linstor",
-    version='0.2.1',
+    version='0.2.2',
     description="Linstor python api",
     long_description="Python linstor api interface",
     url='https://www.linbit.com',
