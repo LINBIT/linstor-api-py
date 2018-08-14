974f09bc90396b8c707763ca065b7f6d7f1e3cfd
Rene Peinthor <rene.peinthor@linbit.com>
2018-08-14 08:18:43 +0200

    SizeCalc: move class from client to linstor api library
    This class is interesting for common use and extension

--- linstor/__init__.py
+++ linstor/__init__.py
@@ -2,6 +2,7 @@ from .linstorapi import LinstorError, LinstorNetworkError, LinstorTimeoutError
 from .linstorapi import ObjectIdentifier
 from .linstorapi import ApiCallResponse, ErrorReport
 from .linstorapi import Linstor
+from .size_calc import SizeCalc
 from . import sharedconsts as consts
 
 VERSION = "0.2.2"
