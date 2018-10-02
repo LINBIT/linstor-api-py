4dcc5834c8bfa6abbfde9f55cdc30114de7630b1
Rene Peinthor <rene.peinthor@linbit.com>
2018-10-02 08:25:59 +0200

    python-linstor 0.6.2

--- linstor/__init__.py
+++ linstor/__init__.py
@@ -7,7 +7,7 @@ from .size_calc import SizeCalc
 from .errors import LinstorError, LinstorTimeoutError, LinstorNetworkError
 from . import sharedconsts as consts
 
-VERSION = "0.6.1"
+VERSION = "0.6.2"
 
 try:
     from linstor.consts_githash import GITHASH
