b23769d3e74f0c86cf07eeff0f9f51ea28ff2605
Roland Kammerer <roland.kammerer@linbit.com>
2019-02-28 10:58:12 +0100

    config: add linstor.Config class
    This is a simple helper class that exposes three functions:
    
    - read_config: uses SafeConfigParser() to parse a config file and
    returns its sections in a dictionary. Can be used directly, is mainly
    used by other functions in this class
    
    - get_section: returns a section (e.g., 'global') from a config file. If
    no config file is specified, this function searches the default LINSTOR
    client configuration files.
    
    - get_controllers: This is a opinionated function that returns output
    that can be used with MultiLinstor. If the environment variable
    'LS_CONTROLLERS' is set, its value will be used. If not, the usual
    config files are searched (call to get_section()), and finally a
    fallback is used (by default linstor://localhost).

--- linstor/__init__.py
+++ linstor/__init__.py
@@ -3,6 +3,7 @@ from .linstorapi import ApiCallResponse, ErrorReport
 from .linstorapi import Linstor, MultiLinstor
 from .resource import Resource, Volume
 from .kv import KV
+from .config import Config
 from .responses import StoragePoolDriver
 from .linstorapi import ResourceData
 from .size_calc import SizeCalc
---
+++ linstor/config.py
@@ -0,0 +1,54 @@
+import linstor
+import os
+
+try:
+    import ConfigParser as configparser
+except ImportError:
+    import configparser
+
+
+class Config(object):
+    @staticmethod
+    def read_config(config_file):
+        cp = configparser.SafeConfigParser()
+        cp.read(config_file)
+        config = {}
+        for section in cp.sections():
+            config[section] = cp.items(section)
+        return config
+
+    @staticmethod
+    def get_section(section, config_file_name=None):
+        home_dir = os.path.expanduser("~")
+        config_file = "linstor-client.conf"
+        user_conf = os.path.join(home_dir, ".config", "linstor", config_file)
+        sys_conf = os.path.join('/etc', 'linstor', config_file)
+
+        config = None
+        if config_file_name and os.path.exists(config_file_name):
+            config = Config.read_config(config_file_name)
+        elif os.path.exists(user_conf):
+            config = Config.read_config(user_conf)
+        elif os.path.exists(sys_conf):
+            config = Config.read_config(sys_conf)
+
+        entries = config.get(section, []) if config else []
+        return {k: v for k, v in entries}
+
+    @staticmethod
+    def get_controllers(section='global', config_file_name=None, fallback='linstor://localhost'):
+        """
+        :param str section Section to parse, defaults to 'global'
+        :param str config_file_name Config file to parse, if not set, the default files in /etc and $HOME are
+         parsed
+        :param str fallback Fallback controller if none was found in the environment or the config. Defaults
+         to 'linstor://localhost'.
+        :return: List of linstor uris. This list is intended to be used via the MultiLinstor class.
+        :rtype: list[str]
+        """
+        controllers = os.environ.get('LS_CONTROLLERS', None)
+        if not controllers:
+            cfg = Config.get_section(section, config_file_name)
+            controllers = cfg.get('controllers', fallback)
+
+        return linstor.MultiLinstor.controller_uri_list(controllers)
