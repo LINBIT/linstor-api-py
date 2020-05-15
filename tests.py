74ca72e8bc3b1842ca7854f653515855be93e6e3
Rene Peinthor <rene.peinthor@linbit.com>
2020-05-15 12:10:13 +0200

    gitlab-ci: use xmlrunner and create junit reports

---
+++ tests.py
@@ -0,0 +1,10 @@
+import unittest
+import sys
+
+if __name__ == '__main__':
+    import xmlrunner
+    loader = unittest.TestLoader()
+    suite = loader.discover(start_dir='linstor_tests')
+    runner = xmlrunner.XMLTestRunner(output='test-reports')
+    res = runner.run(suite)
+    sys.exit(1 if len(res.errors) > 0 else 0)
